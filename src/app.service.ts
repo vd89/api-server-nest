bd68fc5278283021e0f5f69059510cbc616a2b5e
vd89 <dixitvora@gmail.com>
2025-08-20 23:08:47 +0530

    feat: test the commit message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! this is working and for the test commit ';
+    return 'Hello World! This is a NestJS application.';
   }
 }
